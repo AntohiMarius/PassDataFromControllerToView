6934d40109e47b74429b8520497c4424f7172d5d
Antohi Marius <antohi.marius11@hotmail.com>
2021-10-27 22:12:36 +0300

    27.10.2021

---
+++ PassDataFromControllerToView/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using PassDataFromControllerToView.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PassDataFromControllerToView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.FirstName = "Marius";
+            ViewBag.LastName = "Antohi";
+            ViewBag.Address = "antohi.marius11@gmail.com";
+            return View();
+        }
+
+        public ActionResult GetEmployee()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeID = 1,
+                EmployeeName = "Marius Antohi",
+                DateOfJoining = System.DateTime.Now,
+                MartialStatus = 1,
+                IsEligibleForLoan = true,
+                Salary = 15000.00m,
+                CreatedBy = "Admin",
+                CreatedDate = System.DateTime.Now
+            };
+
+            ViewBag.Employee = employee;
+            return View();
+        }
+    }
+}
